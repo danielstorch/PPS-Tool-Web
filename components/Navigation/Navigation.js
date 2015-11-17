149b9694eba1c8096db6c292701a936e1fc058e0
Daniel Storch <mail@danielstorch.de>
2015-11-17 11:57:22 +0100

    Changes to the Navigation

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -10,9 +10,10 @@ import Link from '../Link';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-let AppBar = mui.AppBar
+var AppBar = mui.AppBar
   , LeftNav = mui.LeftNav
-  , MenuItem = mui.MenuItem;
+  , MenuItem = mui.MenuItem
+  , IconButton = mui.IconButton;
  
 export default class Navigation extends React.Component {
  
@@ -21,16 +22,33 @@ export default class Navigation extends React.Component {
  
     this._handleClick = this._handleClick.bind(this);
     this._onLeftNavChange = this._onLeftNavChange.bind(this);
-    this.state = { isDocked: false };
+    this.state = { 
+                    isDocked: false
+                  , iconClassName: "icon-open" 
+                  , currentPage: "Home"
+                 };
   }
  
   _handleClick(e) {
     e.preventDefault();
  
+    //über this.state.isDocked ging es nicht, es hat beim start nicht reagiert
+    var isDocked = !this.state.isDocked;
+
     this.refs.leftNav.toggle();
     this.setState({
       isDocked: !this.state.isDocked,
     });
+
+    if(isDocked){
+      this.setState({
+        iconClassName: "icon-close",
+      });
+    }else{
+      this.setState({
+        iconClassName: "icon-open",
+      });
+    }
   }
  
   _onLeftNavChange(e, key, payload) {
@@ -38,21 +56,27 @@ export default class Navigation extends React.Component {
     this.context.router.transitionTo(payload.route);
   }
 
+  _onLeftNavClick(){
+    this.setState({
+        currentPage: "About",
+    });
+  }
+
   render() {
     return (
       <div id="menu">
-       <AppBar title='PPS-Tool' onLeftIconButtonTouchTap={this._handleClick} style={{"width":"100%" }}/>
-          <LeftNav ref="leftNav" docked={this.state.isDocked} onChange={this._onLeftNavChange} style={{"top":"100% - <AppBar.height>" }}>
-          
-              <MenuItem index={0}>Menu</MenuItem>
-              <MenuItem index={1}>
-                <a className="Navigation-link" href="/" onClick={Link.handleClick}>Home</a>
-              </MenuItem>
-              <MenuItem index={2}>
-                <a className="Navigation-link" href="/about" onClick={Link.handleClick}>About</a>
-              </MenuItem>
-              
-          </LeftNav>
+        <AppBar title="PPS-Tool" iconClassNameLeft={this.state.iconClassName} onLeftIconButtonTouchTap={this._handleClick} style={{"width":"100%" }}
+                iconElementRight={<h1 className="current-page">current-page</h1>}/>
+
+        <LeftNav ref="leftNav" docked={this.state.isDocked} onChange={this._onLeftNavChange} style={{"top":"100% - <AppBar.height>" }}>
+            <MenuItem index={0}>Menu</MenuItem>
+            <MenuItem index={1}>
+              <a className="Navigation-link" href="/" onClick={Link.handleClick}>Home</a>
+            </MenuItem>
+            <MenuItem index={2}>
+              <a className="Navigation-link" href="/about" onClick={Link.handleClick}>About</a>
+            </MenuItem>
+        </LeftNav>
       </div>
     );
   }
